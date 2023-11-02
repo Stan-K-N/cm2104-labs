bd321fd3715599750513e73043863120bb6ff48b
Stan-K-N <s.nedyalkov@rgu.ac.uk>
2023-11-02 09:23:19 +0000

    Added button

---
+++ lab_6/contentlist.js
@@ -0,0 +1,19 @@
+
+function addContent () {
+	// add a list of items to the content div
+    let items = ["hewey", "dewey", "louie"];
+	
+	// build the html string for a <ul> list
+    let items_html = "<ul>";
+    for (let i=0; i < items.length; i++) {
+		item = items[i];
+		items_html += "<li>" + item + "</li>";
+	};
+	items_html += "</ul>";
+	
+	// using javascript
+	// 1. find the content div
+	// 2. modify its html attribute by adding items_html
+
+}
+
---
+++ lab_6/scripts/click.js
@@ -0,0 +1,15 @@
+<button type="button" onclick="go()">
+    Click Me too!
+</button>
+
+<script>
+    function go() {
+        document.getElementById('id1').style.color = 'blue';
+    }
+</script>
+
+<script src="myscript.js"></script>
+
+<button type="button" onclick="goAgain()">
+    Click Me for yellow!
+</button>
